8ab8d5e852952c408930db4021f45169e1ab4035
fan.yao <yaofan29597@163.com>
2017-07-06 14:13:22 +0800

    20170706

---
+++ src/main/scala/Extractor/genPkgActFeat.scala
@@ -0,0 +1,8 @@
+package Extractor
+
+/**
+ * Created by yaofan29597 on 17/6/9.
+ */
+object genPkgActFeat {
+
+}
---
+++ src/main/scala/Extractor/genStats.scala
@@ -0,0 +1,8 @@
+package Extractor
+
+/**
+ * Created by yaofan29597 on 17/4/28.
+ */
+object genStats {
+
+}
